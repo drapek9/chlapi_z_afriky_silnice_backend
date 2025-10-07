a1ba5d50ea15e8dc296b4c0ab0d90aee214f788a
Šimon Drápal <simondrapal9@gmail.com>
2025-10-07 11:32:35 +0200

    zisk požadovaných dat

--- app.py
+++ app.py
@@ -6,4 +6,12 @@ app = Flask(__name__)
 @app.route('/get_road_priorities_for_repair', methods=['POST'])
 def get_road_priorities_for_repair():
     print("Hello world, dostali jsme request na zpracování a strukturu priorit")
+    all_data = request.get_json()
+
+    stavy_silnic = all_data.get("stavy_silnic")
+    historie_oprav_silnic = all_data.get("historie_oprav_silnic")
+
+    if stavy_silnic == None or historie_oprav_silnic == None:
+        return jsonify({"result": False, "reason": "server nezískal požadovaná data"})
+    
     return jsonify({"result": True})
