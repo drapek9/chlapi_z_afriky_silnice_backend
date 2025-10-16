ba56ffc0e4a11ca2846af003b4fd68853ea1c8c4
Šimon Drápal <simondrapal9@gmail.com>
2025-10-16 13:28:30 +0200

    oprava

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def get_road_priorities_for_repair():
 
     sorted_roads = []
     all_stavy = []
+    all_stavy2 = []
+    all_stavy3 = []
 
     response = requests.get(url, params=params)
 
@@ -25,15 +27,27 @@ def get_road_priorities_for_repair():
 
         for one_feature in data["features"]:
             new_road = {}
-            if one_feature["properties"]["stav_sil"] not in ["dobrý", "výborný", "vyhovující"]: # výborný a dobrý stav nepotřebujeme opravovat
-                for inf in ["ozn_trida", "typ_povrchu", "stav_sil"]:
-                    if one_feature["properties"]["stav_sil"] not in all_stavy:
-                        all_stavy.append(one_feature["properties"]["stav_sil"])
-                    new_road[inf] = one_feature["properties"][inf]
-                sorted_roads.append(new_road)
-        
-        print(len(sorted_roads))
+            # if one_feature["properties"]["stav_sil"] not in ["dobrý", "výborný", "vyhovující"]: # výborný a dobrý stav nepotřebujeme opravovat
+            for inf in ["ozn_sil", "ozn_trida", "stav_sil", "ozn_kat"]:
+                if one_feature["properties"]["ozn_kat"] not in all_stavy:
+                    all_stavy.append(one_feature["properties"]["ozn_kat"])
+                if one_feature["properties"]["stav_sil"] not in all_stavy2:
+                    all_stavy2.append(one_feature["properties"]["stav_sil"])
+                if one_feature["properties"]["ozn_trida"] not in all_stavy3:
+                    all_stavy3.append(one_feature["properties"]["ozn_trida"])
+                new_road[inf] = one_feature["properties"][inf]
+            sorted_roads.append(new_road)
+        # print(data["features"])
+        # print(sorted_roads)
         print(all_stavy)
+        print(all_stavy2)
+        print(all_stavy3)
+
+        # ahoj(sorted_roads)
+
+        # get_ai_response(sorted_roads)
+
+        # ozn_trida, typ_povrchu, stav_sil
 
         # for 
         # Uložení do souboru (volitelné)
@@ -44,3 +58,5 @@ def get_road_priorities_for_repair():
     
     return jsonify({"result": True, "data": sorted_roads})
     # return print("Vše proběhlo v pořádku")
+
+# get_road_priorities_for_repair()
